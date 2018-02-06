fb182a444360a2484fe4fde489a5945058b48a67
Yassir Elyakine <yassirelyakine@gmail.com>
2018-02-06 14:59:27 +0000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    var getusername = $('input[type="text"]');
+    var getpassword = $('input[type="password"]');
+    var login = $('#login');
+    var register = $('#register');
+    login.click(function() {
+        if(getusername.val() == account.username && getpassword.val() == account.password) {
+            $('h1').text("Successfull login");
+            console.log("Account loged in");
+        }
+        else {
+            console.log("invalid input");
+        }
+    });
+    register.click(function() {
+        if(getusername.val() && getpassword.val()) {
+            account.username = getusername.val();
+            account.password = getpassword.val();
+            console.log("Account registered")
+            $('h1').text("Successfull register");
+        }
+        else {
+            console.log("Please enter *username* & *password*");
+        }
+    });
+    var account = {
+        username: "yassir",
+        password: "123"
+};
+});
